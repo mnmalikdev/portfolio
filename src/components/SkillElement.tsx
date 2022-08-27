b27f859504f659f5a6a28c373e784ebc704bc3e9
M. Nabeel Malik <mnmalikdev@gmail.com>
2022-08-27 20:48:18 +0500

    Completed Tech Stack Section

--- src/components/SkillElement.tsx
+++ src/components/SkillElement.tsx
@@ -1,49 +1,81 @@
 import Image from "next/image";
-import expressIcon from "./../public/images/express-logo.png";
-import figmaIcon from "./../public/images/logo-figma.png";
-import mongoIcon from "./../public/images/logo-mongo.png";
-import nestIcon from "./../public/images/logo-nest.png";
-import nodeIcon from "./../public/images/logo-node.png";
-import reactIcon from "./../public/images/logo-react.png";
+import expo from "./../public/images/expo.svg";
+import reactNative from "./../public/images/react-native.svg";
 const SkillElement: React.FC = () => {
   return (
     // parent div of entire card
     <>
-      <div className=" flex flex-row justify-evenly items-center my-4">
-        <div className="  mt-3 px-3 w-48 h-44 ">
-          <Image src={reactIcon} />
+      <div className=" flex flex-row justify-evenly items-center my-8">
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> TypeScript </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" height={110} width={110} />
           <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> React </div>
         </div>
-        <div className="   px-3 w-48 h-44  ">
-          <Image src={nodeIcon} />
-          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Node.js </div>
+        <div>
+          <Image src={reactNative} height={110} width={140} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> React-native </div>
         </div>
-        <div className="   px-3 w-48 h-44  ">
-          <Image src={expressIcon} />
-          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Express.js </div>
-        </div>
-        <div className="   px-3 w-48 h-44  ">
-          <Image src={mongoIcon} />
-          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> MongoDB </div>
+        <div>
+          <Image src={expo} height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Expo Cli </div>
         </div>
       </div>
       {/* row 2 */}
       <div className=" flex flex-row justify-evenly items-center mt-11">
-        <div className="  mt-3 px-3 w-48 h-44 ">
-          <Image src={nestIcon} />
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Next.js </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> JavaScript </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Tailwind CSS </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Node.js </div>
+        </div>
+      </div>
+
+      {/* row 3 */}
+      <div className=" flex flex-row justify-evenly items-center mt-11">
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg" height={110} width={110} />
           <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Nest.js </div>
         </div>
-        <div className="   px-3 w-48 h-44  ">
-          <Image src={figmaIcon} />
-          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Figma </div>
-        </div>
-        <div className="   px-3 w-48 h-44  ">
-          <Image src={expressIcon} />
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" height={110} width={110} />
           <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Express.js </div>
         </div>
-        <div className="   px-3 w-48 h-44  ">
-          <Image src={mongoIcon} />
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Git </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Github </div>
+        </div>
+      </div>
+
+      {/* row 4 */}
+      <div className=" flex flex-row justify-center gap-80 items-center mt-11">
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" height={110} width={110} />
           <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> MongoDB </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Firebase </div>
+        </div>
+        <div>
+          <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" height={110} width={110} />
+          <div className=" flex justify-center font-jakarta text-bodyTitleBig text-dark"> Figma </div>
         </div>
       </div>
     </>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,9 +3,9 @@ const Skills: React.FC = () => {
   return (
     <div className=" my-28 ">
       <div className=" flex text-h2 font-bold justify-center items-center">
-        <span className="font-bold font-jakarta text-h2">Skills I Have</span>
+        <span className="font-bold font-jakarta text-h2">My Tech Stack</span>
       </div>
-      <div className="w-full ">
+      <div className="w-full mt-16 ">
         <SkillElement />
       </div>
     </div>
