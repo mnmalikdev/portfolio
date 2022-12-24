4ef893736618b633086a42c0a0ba8c6ffb9c9b1a
M. Nabeel Malik <mnmalikdev@gmail.com>
2022-12-24 18:00:37 +0500

    50 percent responsiveness done attempt 2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,19 +6,27 @@ const About = () => {
         <div className=" text-h2 font-bold text-center my-3 ">About Me</div>
         <div className=" my-5 flex justify-center items-center ">
           <div className=" text-paraGraphMedium w-full lg:text-paraGraphMedium tracking-wide text-paraColor lg:w-2/3 lg:pr-5 ">
-            {""} As a software engineer with expertise in MERN stack development, I have a strong track record of building efficient and scalable web
-            and mobile applications for businesses of all sizes. With experience working with a wide range of clients, I have a proven ability to
-            deliver technical solutions that drive business growth and success. {""}
+            <p>
+              As a software engineer with expertise in MERN stack development, I have a strong track record of building efficient and scalable web and
+              mobile applications for businesses of all sizes. With experience working with a wide range of clients, I have a proven ability to
+              deliver technical solutions that drive business growth and success.
+            </p>
             <br />
             <br />
-            {""} In my current role, I have helped small and medium businesses optimize their processes and scale their operations through the
-            implementation of custom software solutions. My passion for technology and problem-solving drives me to stay up-to-date on the latest
-            industry trends and techniques, ensuring that my clients always receive the best possible service. {""}
+            <p>
+              {" "}
+              In my current role, I have helped small and medium businesses optimize their processes and scale their operations through the
+              implementation of custom software solutions. My passion for technology and problem-solving drives me to stay up-to-date on the latest
+              industry trends and techniques, ensuring that my clients always receive the best possible service.{" "}
+            </p>
             <br />
             <br />
-            {""} If you're in need of a new web application, a mobile app, or simply looking to improve your current software infrastructure, I am
-            ready to provide you with the expertise and guidance you need to succeed. I am confident that my skills and experience make me a strong
-            candidate for your next project and I look forward to the opportunity to work with you. {""}
+            <p>
+              {" "}
+              If you're in need of a new web application, a mobile app, or simply looking to improve your current software infrastructure, I am ready
+              to provide you with the expertise and guidance you need to succeed. I am confident that my skills and experience make me a strong
+              candidate for your next project and I look forward to the opportunity to work with you.{" "}
+            </p>
           </div>
         </div>
       </div>
